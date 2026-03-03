2ca2ed7e43eb41a68d676f5e2eb3212147f9418f
st00pid-cloud <justinemaesecret@gmail.com>
2026-03-03 23:05:07 +0800

    funni shit

--- dialogueInGame/DialogueManagerLogic.gd
+++ dialogueInGame/DialogueManagerLogic.gd
@@ -1,8 +1,8 @@
 extends CanvasLayer
 # Update these paths to match your new tree!
-@onready var name_label = $Control/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/NameLabel
-@onready var text_label = $Control/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/TextLabel
-@onready var portrait_sprite = $Control/PanelContainer/MarginContainer/HBoxContainer/Portrait
+@onready var name_label: Label = $Node2D/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/Label
+@onready var text_label: RichTextLabel = $Node2D/PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/RichTextLabel
+@onready var portrait_sprite: TextureRect = $Node2D/PanelContainer/MarginContainer/HBoxContainer/TextureRect
 
 var current_data: DialogueData
 var line_index: int = 0
@@ -17,6 +17,11 @@ func show_dialogue(data: DialogueData):
 	show()
 	_display_line()
 
+# 1. NEW: Connect your Button's "pressed" signal to this function in the editor
+func _on_next_button_pressed():
+	if is_visible():
+		_handle_progression()
+
 func _display_line():
 	if line_index >= current_data.lines.size():
 		hide()
@@ -29,6 +34,15 @@ func _display_line():
 	tween.tween_property(text_label, "visible_ratio", 1.0, 1.0)
 	line_index += 1
 
+# 2. REFACTOR: Create a shared helper to prevent code duplication
+func _handle_progression():
+	if text_label.visible_ratio < 1.0:
+		# If text is still typing, skip to the end of the line
+		text_label.visible_ratio = 1.0
+	else:
+		# Otherwise, move to the next line
+		_display_line()
+
 func _input(event):
 	if is_visible() and event.is_action_pressed("ui_accept"):
-		_display_line()
+		_handle_progression()
